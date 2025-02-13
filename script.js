ef14747e5ee566c4c3f8ad4b7dc890e2f1437a64
cherryspam <kevindrago01@gmail.com>
2025-02-13 23:27:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -56,14 +56,14 @@
 const messages = [
     "Are you sure?",
     "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
+    "Sherikummm?",
+    "Vaveeeeeee please...",
     "Just think about it!",
     "If you say no, I will be really sad...",
     "I will be very sad...",
-    "I will be very very very sad...",
+    "🥺",
     "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "This Shit will continue as long as you say YEs, Hehehe ❤️"
 ];
 
 let messageIndex = 0;
